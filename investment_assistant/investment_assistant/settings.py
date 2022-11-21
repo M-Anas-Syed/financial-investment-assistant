bf1d00ebca49810d66ec8f37de0f3236babb1237
M A A Syed <maas6@student.le.ac.uk>
2022-11-21 04:30:10 +0000

    database created

--- investment_assistant/investment_assistant/settings.py
+++ investment_assistant/investment_assistant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fia'
 ]
 
 MIDDLEWARE = [
@@ -92,6 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
